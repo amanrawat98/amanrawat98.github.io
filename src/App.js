4e7f8a38dd30fdf1655d327027d68945219d41b5
amanrawat98 <amanrawat2427@gmail.com>
2024-01-17 22:34:51 +0530

    K

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const App = () => {
         <GlobalStyle />
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/amanrawat98.github.io" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="/contact" element={<Contact />} />
