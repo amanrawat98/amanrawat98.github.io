2d3b5037dada890f718288ee1a3fc828d5a25e9a
amanrawat98 <amanrawat2427@gmail.com>
2024-01-18 19:56:07 +0530

    K

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
   return (
     
    <ThemeProvider theme={theme}>
-      <Router basename="/amanrawat98.github.io">
+      <Router >
         <GlobalStyle />
     <Header />
 
